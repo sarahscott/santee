5059381e38d2246b0a3ea893e30f85e6b1242d37
Sarah Scott <sarahoscott@gmail.com>
2019-12-18 17:23:23 -0800

    Updates

---
+++ precache-manifest.b008f15b5785bc975094641f4e53b8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8b4f8974623ea38f99bb089c3f3de6",
+    "url": "/santee/index.html"
+  },
+  {
+    "revision": "88e99aaf7e23befa9ab7",
+    "url": "/santee/static/css/main.5dcd007c.chunk.css"
+  },
+  {
+    "revision": "6ff7785a940eac919d49",
+    "url": "/santee/static/js/2.704471d7.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/santee/static/js/2.704471d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88e99aaf7e23befa9ab7",
+    "url": "/santee/static/js/main.94ecd010.chunk.js"
+  },
+  {
+    "revision": "05fb0ee637a5b0e9e8ad",
+    "url": "/santee/static/js/runtime-main.8bf7c983.js"
+  }
+]);
